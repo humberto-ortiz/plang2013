79965733d62fae7703c440d3920907e7783a8101
Humberto Ortiz-Zuazaga <humberto@hpcf.upr.edu>
2013-01-21 19:28:14 -0400

    Applied GPL to code.

--- racket-examples.rkt
+++ racket-examples.rkt
@@ -2,6 +2,19 @@
 ;;;; my-reduce - implementation of reduce applies a function to consecutive elements of
 ;;;; a list, collecting the result into a single value
 ;;;; Copyright 2012 - Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 ;; Contract: my-reduce : function value list -> value
 ;; Purpose: apply FN to START and all the elements in LST reducing to a single value
@@ -12,5 +25,6 @@
 
 ;;;; examples
 (my-reduce + 0 '(1 2 3))
-
+;; 6
 (my-reduce * 1 '(1 2 3 4 5))
+;; 120
