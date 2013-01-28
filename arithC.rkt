4f4a4c9d1e95def56a89755ece8db8fa88ff70aa
Humberto Ortiz-Zuazaga <humberto@hpcf.upr.edu>
2013-01-28 19:37:27 -0400

    Added if0, a simple conditional, to the core language.

--- arithC.rkt
+++ arithC.rkt
@@ -7,7 +7,9 @@
 (define-type ArithC
   [numC (n : number)]
   [plusC (l : ArithC) (r : ArithC)]
-  [multC (l : ArithC) (r : ArithC)])
+  [multC (l : ArithC) (r : ArithC)]
+  ;; Add conditional
+  [if0C (t : ArithC) (e1 : ArithC) (e2 : ArithC)])
 
 (define (parse [s : s-expression]) : ArithC
   (cond
@@ -17,6 +19,8 @@
        (case (s-exp->symbol (first sl))
          [(+) (plusC (parse (second sl)) (parse (third sl)))]
          [(*) (multC (parse (second sl)) (parse (third sl)))]
+         ;; add conditional
+         [(if0) (if0C (parse (second sl)) (parse (third sl)) (parse (fourth sl)))]
          [else (error 'parse "invalid list input")]))]
     [else (error 'parse "invalid input")]))
 
@@ -27,7 +31,13 @@
   (type-case ArithC a
     [numC (n) n]
     [plusC (l r) (+ (interp l) (interp r))]
-    [multC (l r) (* (interp l) (interp r))]))
+    [multC (l r) (* (interp l) (interp r))]
+    ;; add conditional - note, plai-typed "if" doesn't have the correct semantics
+    ;; we need to expressly test if t = 0
+    [if0C (t e1 e2) (if (= 0 (interp t)) (interp e1) (interp e2))]))
 
 (test (interp (parse '3)) 3)
 (test (interp (parse '(+ (* 1 2) (+ 2 3)))) 7)
+
+(test (interp (parse '(if0 0 1 2))) 1)
+(test (interp (parse '(if0 1 2 3))) 3)
