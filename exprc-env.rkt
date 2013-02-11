d357e6f680bde942ec80229276df2357756029a8
Humberto Ortiz-Zuazaga <humberto@hpcf.upr.edu>
2013-02-11 17:06:15 -0400

    Interpreter with environments.

--- exprc-env.rkt
+++ exprc-env.rkt
@@ -1,5 +1,7 @@
 #lang plai-typed
-;;;; Interpreter for simple arithmetic expressions with functions
+;;;; Interpreter for simple arithmetic expressions with functions and conditionals.
+;;;; This interpeter uses environments to map identifiers to values.
+
 ;;;; Copyright 2012 - Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
 ;;;;
 ;;;; This program is free software: you can redistribute it and/or modify
@@ -25,35 +27,43 @@
   [idC (s : symbol)]
   [appC (fun : symbol) (arg : ExprC)]
   [plusC (l : ExprC) (r : ExprC)]
-  [multC (l : ExprC) (r : ExprC)])
+  [multC (l : ExprC) (r : ExprC)]
+  [if0C (test : ExprC) (e1 : ExprC) (e2 : ExprC)])
+
+;;;; Section 6.1
+;;;; binding identifiers to values
+(define-type Binding
+  [bind (name : symbol) (val : number)])
+ 
+(define-type-alias Env (listof Binding))
+(define mt-env empty)
+(define extend-env cons)
 
 ;; Interpreter that can handle functions
 ;; http://cs.brown.edu/courses/cs173/2012/book/adding-functions.html#%28part._.Growing_the_.Interpreter%29
-(define (interp [e : ExprC] [fds : (listof FunDefC)]) : number
-  (type-case ExprC e
-  [numC (n) n]
-    [idC (_) (error 'interp "shouldn't get here")]
+(define (interp [expr : ExprC] [env : Env] [fds : (listof FunDefC)]) : number
+  (type-case ExprC expr
+    [numC (n) n]
+    [idC (n) (lookup n env)]
     [appC (f a) (local ([define fd (get-fundef f fds)])
-                  (interp (subst a
-                                 (fdC-arg fd)
-                                 (fdC-body fd))
+                  (interp (fdC-body fd)
+                          (extend-env (bind (fdC-arg fd)
+                                            (interp a env fds))
+                                      env)
                           fds))]
-  [plusC (l r) (+ (interp l fds) (interp r fds))]
-  [multC (l r) (* (interp l fds) (interp r fds))]))
+    [plusC (l r) (+ (interp l env fds) (interp r env fds))]
+    [multC (l r) (* (interp l env fds) (interp r env fds))]
+    [if0C (t e1 e2) (if (= 0 (interp t env fds))
+                        (interp e1 env fds)
+                        (interp e2 env fds))]))
 
-;; Substitution
-;; http://cs.brown.edu/courses/cs173/2012/book/adding-functions.html#%28part._.Substitution%29
-(define (subst [what : ExprC] [for : symbol] [in : ExprC]) : ExprC
-  (type-case ExprC in
-    [numC (n) in]
-    [idC (s) (cond
-               [(symbol=? s for) what]
-               [else in])]
-    [appC (f a) (appC f (subst what for a))]
-    [plusC (l r) (plusC (subst what for l)
-                        (subst what for r))]
-    [multC (l r) (multC (subst what for l)
-                        (subst what for r))]))
+;; lookup a name in an environment
+(define (lookup [id : symbol] [env : Env]) : number
+  (cond
+    [(empty? env) (error 'lookup "reference to undefined identifier")]
+    [(cons? env) (cond
+                   [(equal? id (bind-name (first env))) (bind-val (first env))]
+                   [else (lookup id (rest env))])]))
 
 ;; helper function
 (define (get-fundef [n : symbol] [fds : (listof FunDefC)]) : FunDefC
@@ -63,6 +73,14 @@
                    [(equal? n (fdC-name (first fds))) (first fds)]
                    [else (get-fundef n (rest fds))])]))
 
-(test (interp (appC 'const5 (numC 1)) (list (fdC 'const5 '_ (numC 5)))) 5)
+(test (interp (appC 'const5 (numC 1)) mt-env (list (fdC 'const5 '_ (numC 5)))) 5)
 
-(test (interp (appC 'double (numC 3)) (list (fdC 'double 'x (plusC (idC 'x) (idC 'x))))) 6)
+(test (interp (appC 'double (numC 3)) mt-env (list (fdC 'double 'x (plusC (idC 'x) (idC 'x))))) 6)
+
+(test (interp (appC 'cube (numC 3)) mt-env (list (fdC 'cube 'x (multC (idC 'x) (multC (idC 'x) (idC 'x)))))) 27)
+
+;;;; check dynamic scope
+(test/exn (interp (appC 'f1 (numC 3))
+                  mt-env
+                  (list (fdC 'f1 'x (appC 'f2 (numC 4)))
+                        (fdC 'f2 'y (plusC (idC 'x) (idC 'y))))) "undefined identifier")
