744cfc9b266a8e76fac68455c4d2f98e3f85431c
Humberto Ortiz-Zuazaga <humberto@hpcf.upr.edu>
2013-02-11 17:19:46 -0400

    Added parser from class (numbers and binary operations).

--- xinterp.rkt
+++ xinterp.rkt
@@ -6,7 +6,7 @@
 
 (define-type CFWAE
   [num (n : number)]
-  [binop (op : [number number -> number]) (lhs : CFWAE) (rhs : CFWAE)]
+  [binop (op : symbol) (lhs : CFWAE) (rhs : CFWAE)]
   [with (lob : (listof Binding)) (body : CFWAE)]
   [id (name : symbol)]
   [if0 (c : CFWAE) (t : CFWAE) (e : CFWAE)]
@@ -26,10 +26,23 @@
 ;; parse : expression -> CFWAE
 ; This procedure parses an expression into a CFWAE
 (define (parse [sexp : s-expression]) : CFWAE
-  (num 0))
+  (cond
+    [(s-exp-number? sexp) (num (s-exp->number sexp))]
+    [(s-exp-list? sexp)
+     (let ([sl (s-exp->list sexp)])
+       (case (s-exp->symbol (first sl))
+         [(+ - * /) (binop (s-exp->symbol (first sl)) (parse (second sl)) (parse (third sl)))]
+         
+         ;; add conditional
+;         [(if0) (if0C (parse (second sl)) (parse (third sl)) (parse (fourth sl)))]
+         [else (error 'parse "invalid list input")]))]))
 
 ;; interp : CFWAE -> CFWAE-Value
 ;; This procedure evaluates a CFWAE expression, producing a CFWAE-Value.
 (define (interp [expr : CFWAE]) : CFWAE-Value
   (numV 0))
 
+;;;; testing
+(test (parse '3) (num 3))
+(test (parse '(+ 1 2)) (binop '+ (num 1) (num 2)))
+(test (parse '(+ 1 (* 2 3))) (binop '+ (num 1) (binop '* (num 2) (num 3))))
