d6c1ca94edcfecd79e4a10e2b96ad03d380c63d4
elbuo8 <yamil.asusta@upr.edu>
2013-03-20 13:54:19 -0400

    Added NumC, StrC, FalseC, TrueC - Yamil y Alex

--- typed-interp-template.rkt
+++ typed-interp-template.rkt
@@ -12,11 +12,24 @@
 
 (define (interp-full [exprC : ExprC] [env : Env] [store : Store]) : Result
   (type-case ExprC exprC
-    [NumC (n) (v*s (NumV n) store)]
+    [NumC (n) (v*s (NumV n) store)]  ;;Brown
+    [StrC (s) (v*s (StrV s) store)]  ;;Alex Santos
+    [TrueC () (v*s (TrueV) store)]   ;;Yamil Asusta
+    [FalseC () (v*s (FalseV) store)] ;;Yamil Asusta
+    
+    
     [else (interp-error (string-append "Haven't covered a case yet:"
                                        (to-string exprC)))]))
 
 (define (interp [exprC : ExprC]) : ValueC
   (type-case Result (interp-full exprC empty empty)
-    [v*s (value store) value]))
+    [v*s (value store) value]
+    ))
+
+;; Test cases
+
+(test (interp (NumC 5)) (NumV 5))
+(test (interp (StrC "Tu mai' es la gorda")) (StrV "Tu mai' es la gorda"))
+(test (interp (TrueC)) (TrueV))
+(test (interp (FalseC)) (FalseV))
 
