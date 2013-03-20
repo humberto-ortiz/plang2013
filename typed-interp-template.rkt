6e8f206135df981e2c4a40bbc8b46f26cb0f2196
shariff <shariff@Zero.(none)>
2013-03-20 14:15:00 -0400

    FunC anadido

--- typed-interp-template.rkt
+++ typed-interp-template.rkt
@@ -13,6 +13,12 @@
 (define (interp-full [exprC : ExprC] [env : Env] [store : Store]) : Result
   (type-case ExprC exprC
     [NumC (n) (v*s (NumV n) store)]
+    [TrueC () (v*s (TrueV) store)]
+    [FalseC () (v*s (FalseV) store)]
+    [StrC (s)  (v*s (StrV s) store)]
+    [FuncC (Lsym mybody) (v*s (ClosureV Lsym mybody env) store)]
+    
+    
     [else (interp-error (string-append "Haven't covered a case yet:"
                                        (to-string exprC)))]))
 
@@ -20,3 +26,7 @@
   (type-case Result (interp-full exprC empty empty)
     [v*s (value store) value]))
 
+(test (interp (FuncC (list 'x 'y) (StrC "Dembow"))) (ClosureV (list 'x 'y) (StrC "Dembow") empty)) 
+(test (interp (FuncC empty (NumC 4))) (ClosureV empty (NumC 4) empty))  
+;(interp(IfC (FalseC) (NumC 1) (StrC "hola")))
+;(interp (FuncC empty (NumC 4)))
